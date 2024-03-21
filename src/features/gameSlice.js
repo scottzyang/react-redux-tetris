af4dfb6eec8b6dfa7bf70b5495a2befd2a64b184
scottzyang <scottzyang@gmail.com>
2024-03-20 20:26:44 -0500

    move left and right

--- src/features/gameSlice.js
+++ src/features/gameSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { defaultState, canMoveTo, nextRotation } from '../utils'
+import { defaultState, canMoveTo, nextRotation, addBlockToGrid, checkRows, randomShape } from '../utils'
 
 export const gameSlice = createSlice({
   name: 'game',
@@ -7,9 +7,55 @@ export const gameSlice = createSlice({
   reducers: {
     pause: () => {},
     resume: () => {},
-    moveLeft: () => {},
-    moveRight: () => {},
-    moveDown: () => {},
+    moveLeft: (state) => {
+      const { shape, grid, x, y, rotation } = state
+      if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+        state.x = x - 1
+      }
+      return state
+    },
+    moveRight: (state) => {
+      const { shape, grid, x, y, rotation } = state
+      if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+        state.x = x + 1
+      }
+      return state
+    },
+    moveDown: (state) => {
+			const { x, y, shape, grid, rotation, nextShape } = state
+			// Get the next potential Y position
+			const maybeY = y + 1
+			// Check if the current block can move here
+			if (canMoveTo(shape, grid, x, maybeY, rotation)) {
+					// If so move the block
+					state.y = maybeY
+          // In this case we're done return state!
+					return state
+			}
+			// If not place the block
+			const newGrid = addBlockToGrid(shape, grid, x, y, rotation)
+
+			// reset some things to start a new shape/block
+			state.x = 3
+			state.y = -4
+			state.rotation = 0
+			state.grid = newGrid
+			state.shape = nextShape
+			state.nextShape = randomShape()
+
+      // Check that the new block can be added
+			if (!canMoveTo(nextShape, newGrid, 0, 4, 0)) {
+				// If not Game Over
+				console.log("Game Should be over...")
+				state.shape = 0
+				state.gameOver = true
+				return state
+			}
+
+			// Update the score based on if rows were completed or not
+			state.score += checkRows(newGrid)
+			return state
+		},
     rotate: (state) => {
       const { shape, grid, x, y, rotation } = state
       const newRotation = nextRotation(shape, rotation)
