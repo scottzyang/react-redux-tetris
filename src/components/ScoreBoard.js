5eff16bb1f41e77e3c1de2ff4e3f860ce74b6717
scottzyang <scottzyang@gmail.com>
2024-03-27 15:10:01 -0500

    Update color styling

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -10,8 +10,8 @@ export default function ScoreBoard() {
 
   return (
     <div className="score-board">
-      <div>Score: {score}</div>
-      <div>Level: 1</div>
+      <div className="score-text">Score: {score}</div>
+      <div className="score-text">Level: 1</div>
       <button
         className="score-board-button"
         onClick={(e) => {
